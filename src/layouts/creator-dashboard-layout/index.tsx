67b4b906a49adc97355eb8bdadd3c1bdd5655f58
pelummie <amaopelumi96@gmail.com>
2025-07-18 14:38:20 -0300

    Improve mobile responsiveness and add vendor signup form
    Enhanced mobile support for sidebar and tabs, updated layouts for better responsiveness, and improved UI consistency across pages. Added a new vendor signup form component. Removed unused settings tab from fans account.

--- src/layouts/creator-dashboard-layout/index.tsx
+++ src/layouts/creator-dashboard-layout/index.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router-dom";
 import CreatorDashboardHeader from "./header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import CreatorSidebar from "./creator-side-bar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function CreatorDashboardLayout() {
   return (
     <SidebarProvider className="w-full flex flex-col items-center bg-light-gray">
       <CreatorDashboardHeader />
 
-      <main className="w-full flex">
+      <main className="relative w-full flex">
         <CreatorSidebar />
+
+        <SidebarTrigger className="absolute flex md:hidden top-[60px] left-2 text-black" />
 
         <div className="w-full flex flex-col items-center justify-center">
           <Outlet />
