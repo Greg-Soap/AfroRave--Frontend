439b89e02cdc15744b6578ae1c7f79f70221ab8f
Greg-Soap <emmanuelkingsley36@gmail.com>
2025-11-24 13:56:52 +0100

    Refactor RenderEventImage component for improved styling and consistency
    - Adjusted class names to ensure consistent height for images and containers.
    - Enhanced layout by applying height properties directly to the image and container elements for better visual alignment.

--- src/components/shared/render-event-flyer.tsx
+++ src/components/shared/render-event-flyer.tsx
@@ -3,20 +3,24 @@ import { cn } from '@/lib/utils'
 export function RenderEventImage({
   image,
   event_name,
-  className = 'h-[190px] md:h-[312px] rounded-[5px] md:rounded-[15px]',
+  className = ' rounded-[5px] md:rounded-[15px]',
 }: { image?: string; event_name: string; className?: string }) {
   return (
     <>
       {!image || image === '' ? (
         <div
           className={cn(
-            'flex items-center justify-center bg-white/30 px-5 min-h-[190px]',
+            'flex items-center justify-center bg-white/30 px-5 min-h-[190px] h-[190px] md:h-[312px]',
             className,
           )}>
           <p className='text-center font-semibold text-white'>{event_name}</p>
         </div>
       ) : (
-        <img src={image} alt={event_name} className={cn('object-cover', className)} />
+        <img
+          src={image}
+          alt={event_name}
+          className={cn('object-cover h-[190px] md:h-[312px]', className)}
+        />
       )}
     </>
   )
