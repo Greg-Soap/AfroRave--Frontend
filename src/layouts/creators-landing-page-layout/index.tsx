590119e5850ddd40cd7730b45cabdc2ac4434f90
Greg-Soap <emmanuelkingsley36@gmail.com>
2025-12-08 10:00:21 +0100

    Temporarily comment out Footer component in CreatorsLandingPageLayout for post-launch adjustments
    - Commented out the Footer component to be reintroduced after the launch.
    - Added a TODO note for clarity on future implementation.

--- src/layouts/creators-landing-page-layout/index.tsx
+++ src/layouts/creators-landing-page-layout/index.tsx
@@ -1,7 +1,7 @@
 import { AuthModal } from '@/components/auth/auth-modal'
 import { AuthProvider } from '@/contexts/auth-context'
 import { Outlet } from 'react-router-dom'
-import Footer from './sections/footer'
+// import Footer from './sections/footer'
 import Header from './sections/header'
 
 export default function CreatorsLandingPageLayout() {
@@ -14,8 +14,8 @@ export default function CreatorsLandingPageLayout() {
         <main className='w-full min-h-screen flex flex-col items-center gap-[200px] bg-mid-gray'>
           <Outlet />
         </main>
-
-        <Footer />
+        {/* TODO: add back after launch */}
+        {/* <Footer /> */}
       </AuthProvider>
     </div>
   )
