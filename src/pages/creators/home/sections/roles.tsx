1082ed1042519786baafec41dc4b6fe0092ae1a8
Greg-Soap <emmanuelkingsley36@gmail.com>
2025-12-30 21:00:29 +0100

    feat: enhance Hero and Roles sections with authentication modal integration

--- src/pages/creators/home/sections/roles.tsx
+++ src/pages/creators/home/sections/roles.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { useAuth } from '@/contexts/auth-context'
 import { cn } from '@/lib/utils'
 
 export default function RolesSection() {
@@ -13,15 +14,22 @@ export default function RolesSection() {
 }
 
 // Hope added minor fix to match UI
-function RoleDescription({ role, description, className }: IRoles & { className?: string }) {
+function RoleDescription({ role, description, className, login }: IRoles & { className?: string }) {
+  const { openAuthModal } = useAuth()
   return (
-    <div className={cn('w-full md:max-w-1/2 flex flex-col gap-5 md:pb-[290px] xs:pb-[10px]', className)}>
+    <div
+      className={cn(
+        'w-full md:max-w-1/2 flex flex-col gap-5 md:pb-[290px] xs:pb-[10px]',
+        className,
+      )}>
       <div className={cn('w-fit flex flex-col gap-2 font-sf-pro text-white uppercase', className)}>
         <p className='w-fit text-[32px] font-black'>{role}</p>
         <p className='font-normal'>{description}</p>
       </div>
 
-      <Button className='max-w-[120px] h-10 rounded-[20px] font-sf-pro-text text-sm font-semibold px-[17px] py-[11px] uppercase'>
+      <Button
+        onClick={() => openAuthModal('login', login)}
+        className='max-w-[120px] h-10 rounded-[20px] font-sf-pro-text text-sm font-semibold px-[17px] py-[11px] uppercase'>
         Learn More
       </Button>
     </div>
@@ -31,11 +39,13 @@ function RoleDescription({ role, description, className }: IRoles & { className?
 const roles: IRoles[] = [
   {
     role: 'organizers',
+    login: 'creator',
     description:
       'Afro Revive is your backend for seamless event execution. launch events, configure ticket tiers, track live sales, assign vendor slots, and manage guests—all from a unified dashboard. Built for speed, scale, and full control',
   },
   {
     role: 'vendors',
+    login: 'vendor',
     description:
       'With our smart interface, you can browse available service slots, accept bookings, and upload required assets. Stay connected with organizers through in-app messaging (available only on mobile), and manage your event schedule with ease. Everything is streamlined, trackable, and built to help you stay ready and professional at every event.',
   },
@@ -44,4 +54,5 @@ const roles: IRoles[] = [
 interface IRoles {
   role: 'organizers' | 'vendors'
   description: string
+  login: 'creator' | 'vendor'
 }
